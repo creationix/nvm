f3ffea5a40333e8bb5d893b6a529f3d753a566ad
Maksim Ryzhikov <rv.maksim@gmail.com>
2017-12-23 18:39:22 +0300

    chore: improve performance of nvm version command
    fix #1694

--- nvm.sh
+++ nvm.sh
@@ -257,12 +257,7 @@ nvm_tree_contains_path() {
     return 2
   fi
 
-  local pathdir
-  pathdir=$(dirname "${node_path}")
-  while [ "${pathdir}" != "" ] && [ "${pathdir}" != "." ] && [ "${pathdir}" != "/" ] && [ "${pathdir}" != "${tree}" ]; do
-    pathdir=$(dirname "${pathdir}")
-  done
-  [ "${pathdir}" = "${tree}" ]
+  [ "$tree${node_path#$tree}" = "${node_path}" ]
 }
 
 # Traverse up in directory tree to find containing folder
@@ -795,12 +790,9 @@ nvm_ls_current() {
     nvm_add_iojs_prefix "$(iojs --version 2>/dev/null)"
   elif nvm_tree_contains_path "${NVM_DIR}" "${NVM_LS_CURRENT_NODE_PATH}"; then
     local VERSION
-    VERSION="$(node --version 2>/dev/null)"
-    if [ "${VERSION}" = "v0.6.21-pre" ]; then
-      nvm_echo 'v0.6.21'
-    else
-      nvm_echo "${VERSION}"
-    fi
+    local VERSION_SUFFIX=${NVM_LS_CURRENT_NODE_PATH#$NVM_DIR/versions/node/}
+    VERSION=${VERSION_SUFFIX%/bin/node}
+    nvm_echo "${VERSION}"
   else
     nvm_echo 'system'
   fi
